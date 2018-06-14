3f3a98d462fc50c56defd36561b3d33f4f604dfc
thomasschoeftner <thomas.schoeftner@gmail.com>
2018-06-14 17:02:02 +0200

    removed unnecessary comments

--- files/files.go
+++ files/files.go
@@ -4,20 +4,6 @@ import (
 	"os"
 )
 
-//func CheckOrCreateFolder(path string) error {
-//	exists, isFolder, err := Exists(path)
-//	if err != nil {
-//		return err
-//	}
-//
-//	if !exists {
-//		err = CreateFolderStructure(path)
-//	} else if !isFolder {
-//		err = errors.New(fmt.Sprintf("unable to create folder structure \"%s\" because file with same name exists", path))
-//	}
-//	return err
-//}
-
 func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err != nil {
