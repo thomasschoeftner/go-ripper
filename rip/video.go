5432c768d26b01f36f5473315d6d86efff0b864d
thomasschoeftner <thomas.schoeftner@gmail.com>
2019-01-09 16:05:55 +0100

    improved testability of video-ripper

--- rip/video.go
+++ rip/video.go
@@ -5,7 +5,17 @@ import (
 	"go-ripper/ripper"
 	"fmt"
 	"go-ripper/processor"
+	"go-cli/commons"
 )
+
+type RipperFactory func(conf *ripper.HandbrakeConfig, printf commons.FormatPrinter, workDir string) (processor.Processor, error)
+var RipperFactories map[string]RipperFactory
+
+func init() {
+	RipperFactories = make(map[string]RipperFactory)
+	RipperFactories[CONF_RIPPER_HANDBRAKE] = createHandbrakeRipper
+	//TODO initialize more video rippers if required
+}
 
 func RipVideo(ctx task.Context) task.HandlerFunc {
 	conf := ctx.Config.(*ripper.AppConf)
@@ -14,18 +24,19 @@ func RipVideo(ctx task.Context) task.HandlerFunc {
 	var rip processor.Processor
 	var err error
 
-	switch ripperType {
-	case CONF_RIPPER_HANDBRAKE:
-		rip, err = createHandbrakeRipper(conf.Rip.Video.Handbrake, ctx.Printf, conf.WorkDirectory)
-	default:
+	rf := RipperFactories[ripperType]
+	if rf == nil {
 		err = fmt.Errorf("unknown video ripper configured: \"%s\"", ripperType)
+	} else {
+		rip, err = rf(conf.Rip.Video.Handbrake, ctx.Printf, conf.WorkDirectory)
 	}
 
 	if err != nil {
 		return ripper.ErrorHandler(err)
+	} else {
+		return processor.Process(ctx, rip, ripperType,
+			processor.DefaultCheckLazy(ctx.RunLazy, conf.Output.Video),
+			processor.DefaultInputFileFor(conf.Rip.Video.AllowedInputExtensions),
+			processor.DefaultOutputFileFor(conf.Output.Video))
 	}
-	return processor.Process(ctx, rip, ripperType,
-		processor.DefaultCheckLazy(ctx.RunLazy, conf.Output.Video),
-		processor.DefaultInputFileFor(conf.Rip.Video.AllowedInputExtensions),
-		processor.DefaultOutputFileFor(conf.Output.Video))
 }
