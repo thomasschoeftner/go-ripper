c38b0a5308a5b72e25d889b3503ac7af07e542a6
thomasschoeftner <thomas.schoeftner@gmail.com>
2022-11-14 22:15:56 +0100

    Fix docker file after upgrade to go 1.19

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN echo "source /root/functions" >> /root/.bashrc
 WORKDIR /go-ripper/source
 
 # build and install go-ripper
-RUN go get github.com/thomasschoeftner/go-ripper && \
-    cd /go/src/github.com/thomasschoeftner/go-ripper && \
+RUN git clone https://github.com/thomasschoeftner/go-ripper.git && \
+    cd go-ripper && \
     go build -o /usr/bin/go-ripper && \
     rm -rf /go-ripper/source
 
---
+++ run-container.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CONFIG_DIR="$1"
+STORAGE_DIR="$2"
+
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Configuration directory not available at '$CONFIG_DIR'"
+    exit 1
+fi
+
+if [ ! -d "$STORAGE_DIR" ]; then
+    echo "Storage directory not available at '$STORAGE_DIR'"
+    exit 2
+fi
+
+docker run -it --rm --privileged \
+    -v ${CONFIG_DIR}:/go-ripper/config:ro \
+    -v ${STORAGE_DIR}:/go-ripper/storage \
+    go-ripper:latest
