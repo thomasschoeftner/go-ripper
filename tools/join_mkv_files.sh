d92546b2af13d04eeae5c3cfcb5d0d28ac6305e1
thomasschoeftner <thomas.schoeftner@gmail.com>
2022-11-15 14:49:43 +0100

    Add script for joining mkv files

---
+++ tools/join_mkv_files.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+# only works from bash - zsh requires different indexing
+function join_mkvs() {
+    # see: https://stackoverflow.com/questions/58290403/how-to-concatenate-all-mkv-files-using-ffmpeg
+    local all_params=("$@")
+    local output_file="${all_params[0]}"
+    local fragment_files=("${all_params[@]:1}")
+
+    echo "joining fragments: '${fragment_files[@]}' to output file: '$output_file'"
+
+    # build config file for ffmpeg
+    rm -f "${output_file}.ffmpeg"
+    for f in "${fragment_files[@]}"; do
+        echo "file '$f'" >>"${output_file}.ffmpeg"
+    done
+
+    ffmpeg -loglevel info -f concat -safe 0 -i "${output_file}.ffmpeg" -c copy "${output_file}"
+    rm -f "${output_file}.ffmpeg"
+}
+
+join_mkvs "$@"
